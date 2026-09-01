61afaf1d8206af9556b1e3dbe2a11b6b278c6408
U-BSD\465151 <465151@SHS-1ZBF6X3.beaverton.k12.or.us>
2026-09-01 10:23:02 -0700

    Guessing game code

--- guessing.cpp
+++ guessing.cpp
@@ -9,10 +9,12 @@ using namespace std;
 
 int main()
 {
+  //https://www.geeksforgeeks.org/cpp/rand-and-srand-in-ccpp/
   srand(time(NULL));
   int num = rand() % 100;
   int input = 0;
   bool playing = true;
+  char replay = 1;
   
   //cout << num;
   while (playing) {
@@ -25,8 +27,23 @@ int main()
       cout << "You guessed too high" << endl;
 	}
     if (input == num) {
-      cout << "You win!";
-      playing = false;
+      cout << "You win!" << endl;
+      while (replay){
+	cout << "Would you like to play again?(y/n): ";
+	cin >> replay;
+	if (replay == 'y'){
+	  num = rand() % 100;
+	  replay = 0;
 	}
+	else if (replay == 'n'){
+	  playing = false;
+	  replay = 0;
+	}
+	else {
+	  cout << "Incorrect input" << endl;
+	}
+      }
+      replay = 1;
+    }
   }
 }
